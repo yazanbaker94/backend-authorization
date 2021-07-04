75687d0233cff40b93e184efefb7801d34fd70ce
Yazan Baker <yazanbaker94@hotmail.com>
2021-07-04 16:08:22 +0300

    done

--- server.js
+++ server.js
@@ -11,14 +11,37 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
-
   // TODO: 
   // STEP 1: get the jwt from the headers
   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
   // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 
-})
+  const client = jwksClient({
+    // this url comes from your app on the auth0 dashboard 
+    jwksUri: `https://${process.env.AUTH0_DOMAIN_NAME}/.well-known/jwks.json`
+  });
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+// this is a ready to use function
+const getKey=(header, callback)=>{
+    client.getSigningKey(header.kid, function(err, key) {
+        const signingKey = key.publicKey || key.rsaPublicKey;
+        callback(null, signingKey);
+      });
+}
+
+// 'Bearer ;alsdkj;laskd;lkasd;lkl'
+app.get('/authorize',(req,res)=>{
+    const token=req.headers.authorization.split(' ')[1];
+    jwt.verify(token,getKey,{},(err,user)=>{
+        if(err){
+            res.send('invalid token');
+        }
+        res.send(user)
+    })
+    res.send(token);
+});
+
+app.listen(PORT,()=>{
+    console.log(`listening to port: ${PORT}`);
+})
