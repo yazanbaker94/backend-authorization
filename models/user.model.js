47e365ced3f0840baee39e12582bc2236262eb11
Duniaalkilany <duniaalkilany908@gmail.com>
2021-07-07 22:34:58 +0300

    working on it

--- models/user.model.js
+++ models/user.model.js
@@ -21,44 +21,27 @@ const userModel=mongoose.model('user',userSchema)
 
 
 
-const dunia =new userModel({
-    email: 'duniaalkilany908@gmail.com',
-    books: [
-        {
-            name: 'One Hundred Years of Solitude ',
-            description: 'One of the 20th century enduring works, One Hundred Years of Solitude is a widely beloved and acclaimed novel known throughout the world, and the ultimate achievement in a Nobel Prize–winning car..',
-            status: 'Published'
-        },
-
-        {
-            name: 'Hamlet',
-            description: 'The Tragedy of Hamlet, Prince of Denmark, or more simply Hamlet, is a tragedy by William Shakespeare, believed to have been written between 1599 and 1601. The play, set in Denmark, recounts how Pri...',
-            status: 'published'
-        }
-    ]
-});
-
-dunia.save()
-console.log(dunia);
 
 
-    const yazan=new userModel({
-        email: 'softwaredohanow@gmail.com',
-        books: [
-            {
-                name: 'In Search of Lost Time',
-                description: 'also translated as Remembrance of Things Past, novel in seven parts by Marcel Proust, published in Frenc',
-                status: 'available'
-            },
-            {
-                name: 'The Divine Comedy ',
-                description: 'Belonging in the immortal company of the great works of literature, Dante Alighieris poetic masterpiece, The Divine Comedy, is a moving human drama, an unforgettable visionary journey through the ...',
-                status: 'available'
-            }
-        ]
-    });
-    yazan.save()
-    console.log(yazan);
+
+
+    // const yazan=new userModel({
+    //     email: 'softwaredohanow@gmail.com',
+    //     books: [
+    //         {
+    //             name: 'In Search of Lost Time',
+    //             description: 'also translated as Remembrance of Things Past, novel in seven parts by Marcel Proust, published in Frenc',
+    //             status: 'available'
+    //         },
+    //         {
+    //             name: 'The Divine Comedy ',
+    //             description: 'Belonging in the immortal company of the great works of literature, Dante Alighieris poetic masterpiece, The Divine Comedy, is a moving human drama, an unforgettable visionary journey through the ...',
+    //             status: 'available'
+    //         }
+    //     ]
+    // });
+    // yazan.save()
+    // console.log(yazan);
    
 
 
--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const app = express();
 // require mongoose
 const mongoose=require('mongoose');
 
-
+app.use(express.json());
 
 require('dotenv').config();
 const PORT = process.env.PORT ;
 const cors = require('cors');
 app.use(cors()); 
 
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken'
+);
 const jwksClient = require('jwks-rsa');
+//
 
 
 // mongoose  connect to the database at localhost:27017
@@ -22,7 +24,7 @@ mongoose.connect('mongodb://localhost:27017/user',
 );
 
 
-const UserModel = require('./models/user.model')
+const userModel = require('./models/user.model')
 
 
 
@@ -54,19 +56,103 @@ app.get('/authorize',(req,res)=>{
     res.send(token);
 });
 
+function seedingUserShema(){
 
+    const dunia =new userModel ({
+        email: 'duniaalkilany908@gmail.com',
+        books: [
+            {
+                name: 'One Hundred Years of Solitude ',
+                description: 'One of the 20th century enduring works, One Hundred Years of Solitude is a widely beloved and acclaimed novel known throughout the world, and the ultimate achievement in a Nobel Prize–winning car..',
+                status: 'Published'
+            },
+    
+            {
+                name: 'Hamlet',
+                description: 'The Tragedy of Hamlet, Prince of Denmark, or more simply Hamlet, is a tragedy by William Shakespeare, believed to have been written between 1599 and 1601. The play, set in Denmark, recounts how Pri...',
+                status: 'published'
+            }
+        ]
+    });
+    dunia.save()
+console.log(dunia);
+
+}
+seedingUserShema();
+
+
+
+
+
+
+
+
+
+//books end point :get 
 app.get('/books', (req,res) =>{
   const email=req.query.email
   
-  UserModel.findOne({email:email}, (error, books)=>{
+userModel.findOne({email:email}, (error, result)=>{
       if (error){
           res.send(error.message)
       }
-      res.send(books);
+      res.send(result);
    
   });
   
+});
+
+
+
+app.post('/books',(req,res)=>{
+  
+ const {email,name,description,status }=req.body
+userModel.findOne({email:email},(error,data)=>{
+    if (error) {
+        res.send(error.message)
+    } else{
+
+data.books.push({
+name:name,
+description:description,
+status:status
 })
+data.save();
+res.send(data.books)
+    // console.log(data)
+}
+  });
+});
+
+
+app.delete('/books/:index',(req,res)=>{
+  // console.log(req.params)
+  const index = Number(req.params.book_idx);
+  // console.log(index)
+  const {email}=req.query;
+  userModel.findOne({email:email},(error,data)=>{
+  // const mydelet=data[0]
+  //  console.log('data',data)
+  
+  if (error) {
+    res.send(error)
+} else {
+   data.books.splice(Index, 1);
+   data.save();
+    res.send(data.books);
+    // res.send('cat deleted')
+}
+
+});
+});
+
+
+
+
+
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`listening to port: ${PORT}`);
